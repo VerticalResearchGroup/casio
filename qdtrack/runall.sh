e4d6c57dd2d68646cd27cdb83f6a3e027ff20156
Michael Davies <davies@cs.wisc.edu>
2022-10-04 17:23:27 -0500

    Update run script for qdtrack

--- qdtrack/runall.sh
+++ qdtrack/runall.sh
@@ -4,5 +4,14 @@ set -e
 
 CMD="python tools/train.py configs/bdd100k/qdtrack-frcnn_r50_fpn_12e_bdd100k.py"
 
-APP=qdtrack BS=1 NI=30 ./../utils/run_bench.sh $CMD
+APP=qdtrack BS=1  NI=30 ./../utils/run_bench.sh $CMD
+APP=qdtrack BS=2  NI=30 ./../utils/run_bench.sh $CMD
 
+APP=qdtrack BS=1  NI=30 ./../utils/run_prof.sh $CMD
+APP=qdtrack BS=2  NI=30 ./../utils/run_prof.sh $CMD
+
+APP=qdtrack BS=1  NI=30 ./../utils/run_nsys.sh $CMD
+APP=qdtrack BS=2  NI=30 ./../utils/run_nsys.sh $CMD
+
+APP=qdtrack BS=1  NI=1 ./../utils/run_ncu.sh $CMD
+APP=qdtrack BS=2  NI=1 ./../utils/run_ncu.sh $CMD
