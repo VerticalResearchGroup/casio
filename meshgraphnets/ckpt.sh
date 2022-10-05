17e7a2ec98c15ba549d08c324cfccada316a0060
Michael Davies <davies@cs.wisc.edu>
2022-10-05 16:31:03 -0500

    fix ckpt.sh for meshgraphnets

--- meshgraphnets/ckpt.sh
+++ meshgraphnets/ckpt.sh
@@ -2,7 +2,7 @@
 
 python -m meshgraphnets.run_model \
     --mode=train --model=$2 \
-    --dataset_dir=/datasets/$1 \
+    --dataset_dir=./meshgraphnets/datasets/$1 \
     --checkpoint_dir=/tmp/$1-$2 --execstyle='createchkpoint' \
     --num_training_steps=10000
 
