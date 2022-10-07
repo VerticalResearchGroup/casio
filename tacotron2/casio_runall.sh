44c38d5f7393c3278cd556eb7ea3328337b620c3
Karu Sankaralingam <karu@cs.wisc.edu>
2022-10-07 16:31:25 -0500

    added bs=1 for tactotron2

--- tacotron2/casio_runall.sh
+++ tacotron2/casio_runall.sh
@@ -10,5 +10,6 @@ for BS in 64 32 16 8 4 2 1; do
 	APP=tacotron2 BS=$BS NW=10 NI=10 ../utils/run_nsys.sh $RUN_CMD
 done
 
-APP=tacotron2 BS=64 NW=10 NI=1 ../utils/run_ncu.sh $RUN_CMD
 
+APP=tacotron2 BS=64 NW=10 NI=1 ../utils/run_ncu.sh $RUN_CMD
+APP=tacotron2 BS=1 NW=10 NI=1 ../utils/run_ncu.sh $RUN_CMD
