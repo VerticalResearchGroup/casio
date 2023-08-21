b839a8d50648ecfaabee928afcc157097c092610
Michael Davies <davies@cs.wisc.edu>
2023-08-21 16:10:15 -0500

    Fix accidentally commenting out something

--- mingpt/runall.sh
+++ mingpt/runall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# APP=gpt3 BS=1 NW=30 NI=30 $CASIO/utils/run_bench.sh python tests/casio_bench.py
-# APP=gpt3 BS=1 NW=30 NI=30 $CASIO/utils/run_prof.sh python tests/casio_bench.py
+APP=gpt3 BS=1 NW=30 NI=30 $CASIO/utils/run_bench.sh python tests/casio_bench.py
+APP=gpt3 BS=1 NW=30 NI=30 $CASIO/utils/run_prof.sh python tests/casio_bench.py
 APP=gpt3 BS=1 NW=30 NI=30 $CASIO/utils/run_nsys.sh python tests/casio_bench.py
 APP=gpt3 BS=1 NW=30 NI=1  $CASIO/utils/run_ncu.sh python tests/casio_bench.py
